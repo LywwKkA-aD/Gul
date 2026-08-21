97a130e09e64e14f6d4b710164a5c9253e659e7b
LywwKkA-aD <129105081+LywwKkA-aD@users.noreply.github.com>
2026-08-22 00:57:19 +0200

    [fix] make CI portable across Linux and Windows

--- internal/dsp/opus/opus.go
+++ internal/dsp/opus/opus.go
@@ -13,6 +13,7 @@ package opus
 /*
 #cgo CFLAGS: -O2 -DHAVE_CONFIG_H -I${SRCDIR} -I${SRCDIR}/../../../third_party/opus/include -I${SRCDIR}/../../../third_party/opus/celt -I${SRCDIR}/../../../third_party/opus/silk -I${SRCDIR}/../../../third_party/opus/silk/float -I${SRCDIR}/../../../third_party/opus/src
 #cgo darwin CFLAGS: -mmacosx-version-min=11.0
+#cgo linux LDFLAGS: -lm
 #include <opus.h>
 */
 import "C"
